ecdb5858cfa45e3e60087faf3db952e7caaefcf3
Riyaskansa <96871709+Riyaskansa@users.noreply.github.com>
2022-01-13 10:42:34 +0530

    Test

---
+++ Automation.java
@@ -0,0 +1,23 @@
+package week3.day2;
+
+public class Automation extends Multi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      Automation obj = new Automation();
+      obj.python();
+      obj.ruby();
+
+	}
+
+	public void selenium() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void java() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ Multi.java
@@ -0,0 +1,15 @@
+package week3.day2;
+
+public abstract class Multi implements Testtool {
+public void python() {
+	System.out.println("Python");
+}
+public void ruby() {
+	System.out.println("Ruby");
+}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
